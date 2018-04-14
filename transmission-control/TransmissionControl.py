4dd03aee86678855f5a2a28aec8e91d6f50fa4ae
Doug Barry <github@gtko.co.uk>
2018-04-14 09:02:12 +0100

    Added support for default tracker set
    Default tracker set support added. Made adjustments to argument parsing for clarity. Included example trackers list file.

--- transmission-control/TransmissionControl.py
+++ transmission-control/TransmissionControl.py
@@ -59,8 +59,8 @@ class TransmissionControl(object):
     def load_rules(self, fname):
         rules = {}
         try:
-            with open(fname) as json_file:
-                data = json.load(json_file)
+            with open(fname) as f:
+                data = json.load(f)
                 for rule in data:
                     pattern = rule['pattern']
                     destination = rule['destination']
@@ -72,6 +72,17 @@ class TransmissionControl(object):
             self.logger.info("Failed to load rules from: " + fname)
             return None
         return rules
+
+    def load_trackers(self, fname):
+        trackers = []
+        try:
+            with open(fname) as f:
+                trackers = f.read().splitlines()
+        except Exception as e:
+            self.logger.exception(e)
+            self.logger.info("Failed to load trackers list from: " + fname)
+            return None
+        return trackers
 
     def save_rules(self, fname, rules):
         json.dump(rules, fname)
@@ -120,6 +131,9 @@ class TransmissionControl(object):
 
             self.silent = configuration.get("silent", False)
             self.logger.debug("Using silent flag: " + str(self.silent))
+
+            self.trackers_list = configuration.get("trackers_list", None)
+            self.logger.debug("Tracker list contains " + str(len(self.trackers_list)) + " entries.")
         except Exception as e:
             raise TransmissionControlConfigurationError("Failed to interpret configuration dictionary", e)
 
@@ -164,6 +178,19 @@ class TransmissionControl(object):
                 continue
 
             for torrent in torrents:
+
+                try:
+                    if trackers_list is not None:
+                        if len(trackers_list) >= 1:
+                            this_torrents_new_trackers = []
+                            for ftracker in trackers_list:
+                                if len(filter (lambda x : x == ftracker, torrent.trackers)) == 0:
+                                    this_torrents_new_trackers.append(ftracker)
+
+                            if len(this_torrents_new_trackers) > 0:
+                                tc.change_torrent(ids=torrent.id, trackerAdd=this_torrents_new_trackers)
+                except Exception as e:
+                    pass
 
                 result = repattern.search(torrent.downloadDir)
                 if result is None:
@@ -247,22 +274,44 @@ class TransmissionControl(object):
 
 if __name__ == '__main__':
 
-    parser = argparse.ArgumentParser(description='Launch options')
+    parser = argparse.ArgumentParser(prog=os.path.basename(sys.argv[0]), description='Launch options')
 
-    parser.add_argument('-d', '--dry-run', help='Test only, perform no actions', action='store_true', dest='dry_run', default=False)
-    parser.add_argument('-t', '--host-address', help='Transmission-daemon host address. Default: localhost', type=str, dest='host_address', default="localhost")
-    parser.add_argument('-p', '--host-port', help='Transmission-daemon host port. Default: 9091', type=int, dest='host_port', default=9091)
-    parser.add_argument('-u', '--username', help='RPC username. Default: transmission', type=str, dest='username', default='transmission')
-    parser.add_argument('-w', '--password', help='RPC password. Default: password', type=str, dest='password', default='password')
-    parser.add_argument('-r', '--rules', help='Move rules input file name', type=str, dest='move_rules_file', default=None)
-    parser.add_argument('-e', '--rules-example', help='Output rules example file to stdout', action='store_true', dest='rules_example', default=False)
-    parser.add_argument('-s', '--suppress-warnings', help='Suppress warnings about destination directories', action='store_true', dest='suppress_move_warnings')
+    connection_group = parser.add_argument_group(title='Connection settings')
 
-    group = parser.add_mutually_exclusive_group()
-    group.add_argument('-v', '--verbose', help='Verbose output', action='store_true', dest='verbose', default=False)
-    group.add_argument('-q', '--quiet', help='Suppress all logging output. Silent mode.', action='store_true', dest='silent', default=False)
+    connection_group.add_argument('-t', '--host-address', help='Transmission-daemon host address. Default: localhost',
+                                  type=str, dest='host_address', default="localhost")
+    connection_group.add_argument('-p', '--host-port', help='Transmission-daemon host port. Default: 9091', type=int,
+                                  dest='host_port', default=9091)
+    connection_group.add_argument('-u', '--username', help='RPC username. Default: transmission', type=str,
+                                  dest='username', default='transmission')
+    connection_group.add_argument('-w', '--password', help='RPC password. Default: password', type=str, dest='password',
+                                  default='password')
 
-    args = parser.parse_args()
+    debug_group = parser.add_argument_group(title='Debug settings')
+
+    debug_group.add_argument('-d', '--dry-run', help='Test only, perform no actions', action='store_true',
+                             dest='dry_run', default=False)
+
+    debug_group2 = debug_group.add_mutually_exclusive_group()
+    debug_group2.add_argument('-v', '--verbose', help='Verbose output', action='store_true', dest='verbose',
+                              default=False)
+    debug_group2.add_argument('-q', '--quiet', help='Suppress all logging output. Silent mode.', action='store_true',
+                              dest='silent', default=False)
+
+    rules_group = parser.add_argument_group(title='Rules operations')
+
+    rules_group.add_argument('-r', '--rules-file', help='Rules definition input file', type=str, dest='move_rules_file',
+                             default=None)
+    rules_group.add_argument('-e', '--example', help='Output example rules file to stdout', action='store_true',
+                             dest='rules_example', default=False)
+    rules_group.add_argument('-s', '--suppress-warnings', help='Suppress warnings about destination directories',
+                             action='store_true', dest='suppress_move_warnings')
+
+    trackers_group = parser.add_argument_group(title='Trackers operations')
+
+    trackers_group.add_argument('-a', '--trackers-file', help='Default extra trackers input file', type=str,
+                                dest='tracker_file', default='')
+
 
     rootLogger = logging.getLogger()
     if args.verbose:
@@ -281,6 +330,7 @@ if __name__ == '__main__':
     tcontrol = TransmissionControl()
 
     move_rules = {}
+    trackers_list = []
     tconfiguration = {}
 
     if args.rules_example:
@@ -297,11 +347,31 @@ if __name__ == '__main__':
         tcontrol.save_rules(sys.stdout, example_rules)
         sys.exit(0)
 
+    operation_rules = False
+    operation_trackers = False
+
     if args.move_rules_file is not None:
-        if not os.path.isfile(args.move_rules_file):
-            logging.error("Move rules file is not valid")
-        else:
+        if os.path.isfile(args.move_rules_file):
             move_rules = tcontrol.load_rules(args.move_rules_file)
+            operation_rules = True
+        else:
+            operation_rules = False
+    else:
+        operation_rules = False
+
+    if args.tracker_file is not None:
+        if os.path.isfile(args.tracker_file):
+            trackers_list = tcontrol.load_trackers(args.tracker_file)
+            operation_trackers = True
+        else:
+            operation_trackers = False
+    else:
+        operation_trackers = False
+
+    if (operation_rules is False) and (operation_trackers is False):
+        logging.error('No operations to carry out.')
+        parser.print_help()
+        sys.exit(2)
 
     tconfiguration = {
         "dry_run": args.dry_run,
@@ -313,9 +383,10 @@ if __name__ == '__main__':
         "password": args.password,
         "move_rules": move_rules,
         "suppress_move_warnings": args.suppress_move_warnings,
+        "trackers_list": trackers_list
     }
 
-    logging.info(tcontrol.name + " started")
+    logging.info(os.path.basename(sys.argv[0]) + " started")
 
     try:
         tcontrol.run(tconfiguration)
@@ -323,4 +394,4 @@ if __name__ == '__main__':
         logging.exception(e)
         sys.exit(1)
 
-    logging.info(tcontrol.name + " finished")
+    logging.info(os.path.basename(sys.argv[0]) + " finished")
